d4f9976c305d608fefc570ecada0b6f2d86f9066
vectorbatesta <vectorbatistalol@gmail.com>
2023-11-03 06:19:22 -0300

    read full file to array of 261488 elements

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    FILE *arq = fopen("heart_beat.txt", "r");
-    FILE *arq_out = fopen("heart_beat.mp3", "w");
+int main(int argc, char* argv[]){
+    //inicializa arquivos
+    FILE *arq;
+    FILE *arq_out;
+
+    if (argc > 1){ //se foi passado por argc e argv[]
+        char const* const nomeArq = argv[1];
+        arq = fopen(nomeArq, "r");
+    }
+    else{ //se nao, apenas abrir
+        arq = fopen("heart_beat.txt", "r");
+    }
+
+    arq_out = fopen("heart_beat.mp3", "w");
+
+    //teste se o arquivo está na pasta do projeto
     if (arq == NULL){
         printf("erro arquivo entrada");
         return(EXIT_FAILURE);
@@ -12,15 +25,55 @@ int main(){
         printf("erro arquivo saida");
         return(EXIT_FAILURE);
     }
+    
+
+    //atribuiçao da linha de leitura do arquivo
+    char linha[999];
+
+    int quantLinhas = 0;
+    //pega quantidade de valores (no heart_beat.txt há 261488)
+    while(fgets(linha, sizeof(linha), arq)){ //false se EOF
+        quantLinhas++;
+    }
+    printf("Quantidade de linhas no arquivo: %i\n", quantLinhas); //deve printar 261488
+    rewind(arq); //volta pro começo do arq
 
 
-    int Fs = 44100;
+    int *valores = (int*) malloc(quantLinhas * sizeof(int)); //aloca tamanho de 261488
+   
+    for(int i = 0; fgets(linha, sizeof(linha), arq); i++){ //false se EOF
+        valores[i] = atoi(linha); //atribui para int
+    }
+
+    //printa TODOS OS 261488 VALORES! - apenas para teste
+    // for(int i = 0; i < quantLinhas; i++){
+    //     printf("%i, ", valores[i]);
+    // }
+
+
+
+
+
+
+
+
+
+
+    int Fs = 44100; //frequency
 
     char *audio_arquivo;
+    audio_arquivo = "test"; //teste
 
-    audio_arquivo = "test";
+    fprintf(arq_out, audio_arquivo); //teste print para escrever string em mp3
 
-    fprintf(arq_out, audio_arquivo);
 
+
+
+
+
+
+    //termina programa fechando arquivos
+    fclose(arq);
+    fclose(arq_out);
     return EXIT_SUCCESS;
 }
